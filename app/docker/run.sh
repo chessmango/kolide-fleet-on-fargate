1ab45630895ee8f294a3e9e0d1bcdfc64423082a
Owen Valentine <owenvalentine@gmail.com>
2020-08-11 16:45:31 +0200

    new tls branch

--- app/docker/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-AWSREGION=$(curl -s $ECS_CONTAINER_METADATA_URI/task | jq -r .AvailabilityZone | sed 's/.$//')
-
-aws acm get-certificate --region $AWSREGION --certificate-arn $KOLIDE_CERTIFICATE_ARN | jq -r .CertificateChain > /certs/kolide.crt
-aws ssm get-parameters --region $AWSREGION --names $KOLIDE_SSL_PRIVKEY_SSM_PARAM --with-decryption | jq -r .Parameters[0].Value > /certs/kolide.key
-fleet prepare db
-fleet serve
